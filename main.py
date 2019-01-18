152bd4e80e933098f04179b5d1ef2cdb0ecdb207
Node <jordansavell1198@gmail.com>
2019-01-18 13:20:47 +0000

    Updated main.py & gitignore

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
 import hosts_file
 
 
-class Main():
-    hosts = hosts_file
+class Main:
 
-    def run(self):
+    hosts = hosts_file.Entries()
+
+    def launch(self):
         while True:
             try:
-                if Menu(1):
-                    print("You selected 1, initiating")
-                    self.hosts.import_file_entry('hosts_append')
+                if self.menu(1):
+                    self.hosts.prompt_entry()
                     break
-                if Menu(2):
-                    print("You selected 2")
-                if Menu(3):
-                    print("You selected 3")
-                if Menu(4):
-                    print("You selected 4")
-                if Menu(5):
-                    print("You selected 5")
-                if Menu(6):
-                    print("You selected 6")
+                elif self.menu(2):
+                    break
+                elif self.menu(3):
+                    break
+                elif self.menu(4):
+                    break
+                elif self.menu(5):
+                    break
+                elif self.menu(6):
+                    break
                 else:
-                    print("You didn't select a value, exiting.")
+                    print("Something happened, exiting.")
                     break
             except ValueError as val:
-                print("Expected int, received " + str(val))
+                print("Expected int, received " + str(val) + "\nGoing back to menu.")
 
-
-def Menu(menu_choice):
+    def menu(self, menu_choice):
         print("""
-        [1] - Add Host Entries\n
-        [2] - Disable Adobe Services\n
-        [3] - Replace AMTLIB.dll\n
-        [4] - Edit Firewall\n
-        [5] - Disable Adobe Startup applications\n
-        [6] - Perform complete patch (automatically goes from 1 through 5 for you)\n""")
+[1] - Add Host Entries\n
+[2] - Disable Adobe Services\n
+[3] - Replace AMTLIB.dll\n
+[4] - Edit Firewall\n
+[5] - Disable Adobe Startup applications\n
+[6] - Perform complete patch (automatically goes from 1 through 5 for you)\n""")
         menu_choice = int(input("Select action to perform via integer: "))
         return menu_choice
 
 
 if __name__ == "__main__":
-    Main().run()
+    Main().launch()
