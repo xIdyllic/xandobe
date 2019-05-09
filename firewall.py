5688a89c12fef4a2fdb949a38d3c8d6e39522e45
Node <jordansavell1198@gmail.com>
2019-05-09 11:29:30 +0100

    Slightly cleaned up firewall and admin check

--- firewall.py
+++ firewall.py
@@ -1,5 +1,5 @@
 from python_hosts import utils
-import logging, subprocess, os
+import logging, subprocess, os, ctypes
 
 '''Powershell will be run through subprocess module to allow diversity for rules and net administration.
 netsh was the first consideration until finding out that they might be deprecating it in future Windows versions:
@@ -12,41 +12,43 @@ use netsh to configure and manage Windows Firewall with Advanced Security."
  
 This is preferable, though.'''
 
+
 class Firewall:
 
+    system = os
     subproc = subprocess
     logger = logging
     logger.basicConfig(filename="adobe_patcher.log", filemode='a', level=logging.INFO)
-    temp_folder = os.getenv('temp')
-
-    ps_file = "{}\\xandobe\\admin_check.ps1".format(temp_folder)
-    ps_script = subproc.Popen(["powershell.exe", ps_file], stdout=subproc.PIPE)
-    ps_commands = ['$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())',
-                   '$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)']
+    ps_file_inbound = 'powershell/add_rule_inbound.ps1'
+    ps_file_outbound = 'powershell/add_rule_outbound.ps1'
 
     '''Much love, servervault. https://serverfault.com/questions/11320/command-line-safety-tricks/29261#29261'''
 
     def prompt_firewall(self):
         try:
-            if not os.path.exists(self.temp_folder + '\\xandobe'):
-                os.mkdir(self.temp_folder + '\\xandobe')
+            if ctypes.windll.shell32.IsUserAnAdmin():
+                self.logger.info(" User is admin, adding inbound firewall rule...")
+                try:
+                    self.subproc.Popen(["powershell.exe", self.ps_file_inbound], stdout=self.subproc.PIPE)
+                    self.logger.log(self.logger.INFO, " Firewall rule added to inbound. Adding to outbound...")
+                    try:
+                        self.logger.info("Attempting to add Outbound")
+                        self.subproc.Popen(['powershell.exe', self.ps_file_outbound], stdout=self.subproc.PIPE)
+                    except (PermissionError, OSError) as err:
+                        self.logger.exception(err)
+                except (PermissionError, OSError) as err:
+                    self.logger.exception(err)
             else:
-                with open(self.ps_file, "w+") as io:
-                    for command in self.ps_commands:
-                        io.write("%s\n" % command)
-                        self.logger.info("PowerShell commands inserted into file C:\\admin_check.ps1 - Continuing")
-                    io.close()
-                output = self.ps_script.communicate()[0]
-                if output.decode('ascii') == 'True':
-                    pass # Add Firewall rules.
-                if output.decode('ascii') == 'False':
-                    print('False permissions! Check adobe_patcher.log for more details.')
-                    self.logger.exception('You do not have the correct permissions! Returned false.')
-                    raise PermissionError
-                else:
-                    print("Critical error, exiting.")
-                    self.logger.critical("Something happened, exiting.") # Could keep this or leave the raise to do it's thing. We'll see.
-                    exit()
-        except (PermissionError, OSError, AttributeError) as error:
-            print("There was an error. Most likely permissions. Check adobe_patcher.log")
+                print("Permission error! Check the log file. Exiting.")
+                self.logger.error(" User is not admin, exiting.")
+                exit()
+        except (OSError, OverflowError, SystemError) as error:
             self.logger.exception(error)
+
+    def get_path(self, paths, executable=''):
+        adobe_paths = []
+        for dir in self.system.walk(paths):
+            print(dir)
+            exit()
+
+
