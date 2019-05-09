cc437e4a018683ba15f35fe10fdf238f89f353ce
Node <jordansavell1198@gmail.com>
2019-05-09 11:29:46 +0100

    added ps scripts for firewall rules (not fully done)

---
+++ powershell/add_rule_outbound.ps1
@@ -0,0 +1,13 @@
+﻿$firewall_args = @{ 
+ 
+    'DisplayName' = 'Adobe_PATCH';
+    'Program' = 'C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe';
+    'Action' = 'Block';
+    'Profile' = 'Domain, Private'
+    'Description' = 'Fucking test';
+    'Protocol' = 'TCP';
+    'Direction' = 'Outbound'
+     
+    }
+
+New-NetFirewallRule @firewall_args
